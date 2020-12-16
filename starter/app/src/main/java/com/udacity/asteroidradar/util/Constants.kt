35ce93491cb8c7c422bc764694f895df9c3d7210
EverE <everesparza@outlook.com>
2020-12-16 14:37:35 -0600

    Hiding the API KEY

--- starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
@@ -1,8 +1,10 @@
 package com.udacity.asteroidradar.util
 
+import com.udacity.asteroidradar.BuildConfig
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "OQPXg5akNHGNe3wvF93MCO0ltFWNFNYUgAm6BVh7"
+    const val API_KEY = BuildConfig.API_KEY
 }
